7fa90d1175bac630eb661a9df4c2ba3149e0940c
SheiUn <me.sheiun@gmail.com>
2019-02-18 23:36:20 +0800

    feat: add new available function

--- modules/school.py
+++ modules/school.py
@@ -51,6 +51,8 @@ class NTUST(AbstractSchool):
     CHECK_URL = f'{PREFIX_URL}ChooseDoc.aspx'
     ERROR_URL = f'{PREFIX_URL}hacker_page.aspx'
 
+    QUERY_URL = 'https://querycourse.ntust.edu.tw/QueryCourse/QueryCourseInfo'
+
     VIEW_STATE = b64encode(' '.encode()).decode()
 
     task_list = list()
@@ -76,6 +78,7 @@ class NTUST(AbstractSchool):
         return text
 
     def login(self, data):
+        self.login_data = data
         data["code_box"] = self.verification_code
         data["Button1"] = "登入系統"
         data["__VIEWSTATE"] = NTUST.VIEW_STATE
@@ -88,7 +91,7 @@ class NTUST(AbstractSchool):
     def logout(self):
         return super().logout()
 
-    def is_availiabe(self, course_id):
+    def is_available(self, course_id):
         data = {'courseno': course_id,
                 'Button1': '查詢',
                 '__VIEWSTATE': NTUST.VIEW_STATE,
@@ -102,6 +105,19 @@ class NTUST(AbstractSchool):
         restrict = get_value(html.xpath('//*[@id="restrict2"]/font/text()')[0])
         now = get_value(html.xpath('//*[@id="now_peop"]/font/text()')[0])
         return restrict > now
+
+    @staticmethod
+    def is_available_by_query(course_id):
+        data = requests.post(NTUST.QUERY_URL, data={'semester': '1072',
+                                                    'course_no': course_id,
+                                                    'course_name': '',
+                                                    'course_teacher': '',
+                                                    'nodes': 'M1,M2,M3,M4,M5,M6,M7,M8,M9,M=,MA,MB,MC,MD,T1,T2,T3,T4,T5,T6,T7,T8,T9,T=,TA,TB,TC,TD,W1,W2,W3,W4,W5,W6,W7,W8,W9,W=,WA,WB,WC,WD,R1,R2,R3,R4,R5,R6,R7,R8,R9,R=,RA,RB,RC,RD,F1,F2,F3,F4,F5,F6,F7,F8,F9,F=,FA,FB,FC,FD,S1,S2,S3,S4,S5,S6,S7,S8,S9,S=,SA,SB,SC,SD,U1,U2,U3,U4,U5,U6,U7,U8,U9,U=,UA,UB,UC,UD',
+                                                    'dimension': '',
+                                                    'course_notes': '',
+                                                    'only_general': '0',
+                                                    'only_ntust': '0'}).json()[0]
+        return data["Restrict2"] > data["ChooseStudent"]
 
     def verify_to_choose(self):
         self.session.post(
@@ -146,18 +162,20 @@ class NTUST(AbstractSchool):
 
         def _thread(arg):
             while True:
+                print(f'{arg["course_id"]} is running...')
                 if arg["listen"]:
-                    if arg["cls"].is_availiabe(arg["course_id"]):
-                        if arg["cls"].choose(course_id):
+                    if arg["cls"].is_available_by_query(arg["course_id"]):
+                        self.login(self.login_data)
+                        if arg["cls"].choose(arg["course_id"]):
                             break
                 else:
-                    if arg["cls"].choose(course_id):
+                    if arg["cls"].choose(arg["course_id"]):
                         break
 
                 sleep(arg["delay"])
 
         task = Thread(target=_thread, args=({'cls': self, 'course_id': course_id,
-                                            'listen': listen, 'delay': delay}, ))
+                                             'listen': listen, 'delay': delay}, ))
         self.task_list.append(task)
         task.start()
 
